95f13929d474ab8628c5a0fd097f7ea6f72d255c
Gleb Valin <the7winds@yandex.ru>
2017-10-29 17:50:47 +0300

    add hw and tests

--- src/main/java/ru/spbau/mit/main.kt
+++ src/main/java/ru/spbau/mit/main.kt
@@ -1,14 +1,70 @@
 package ru.spbau.mit
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    
-    words.add("world!")
+import java.util.*
+import kotlin.collections.HashMap
+import kotlin.collections.HashSet
 
-    return words.joinToString(separator = " ")
+
+/**
+ * NameStorage is a wrapper around a name
+ * @name - repeatable part of the name
+ * @times - times to repeat
+ */
+class NameStorage(name: String, times: Int) {
+    private val orders: Map<Char, ArrayList<Int>>
+    private val kString = StringBuilder()
+    private val removed = HashSet<Int>()
+
+    init {
+        for (i in 1..times) {
+            kString.append(name)
+        }
+
+        val mutOrders: HashMap<Char, ArrayList<Int>> = HashMap()
+        kString.forEachIndexed { index, c ->
+            val order = mutOrders.getOrPut(c, { ArrayList() } )
+            order.add(index)
+        }
+
+        orders = mutOrders
+    }
+
+    /**
+     * @property name returns current name value
+     */
+    val name: String
+        get() = kString.filterIndexed { index, _ -> !removed.contains(index) }.toString()
+
+    /**
+     * removes [order] occurrence of character [c]
+     */
+    fun remove(c: Char, order: Int) {
+        val order = order - 1
+        val toRemove = orders[c]?.get(order)
+
+        when (toRemove) {
+            null -> throw Exception("incorrect command")
+            else -> {
+                orders[c]?.removeAt(order)
+                removed.add(toRemove)
+            }
+        }
+    }
 }
 
 fun main(args: Array<String>) {
-    println(getGreeting())
+    val scanner = Scanner(System.`in`)
+    val times = scanner.nextInt()
+    val name = scanner.next()!!
+    val nameStorage = NameStorage(name, times)
+
+    val n = scanner.nextInt()
+
+    for (i in 1..n) {
+        val order = scanner.nextInt()
+        val c = scanner.next()?.get(0)!!
+        nameStorage.remove(c, order)
+    }
+
+    println(nameStorage.name)
 }
--- src/test/java/ru/spbau/mit/test.kt
+++ src/test/java/ru/spbau/mit/test.kt
@@ -1,10 +1,54 @@
 package ru.spbau.mit
-import kotlin.test.assertEquals
 import org.junit.Test
+import kotlin.test.assertEquals
 
 class TestSource {
     @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+    fun testSimpleName() {
+        val nameStorage = NameStorage("abc", 2)
+
+        assertEquals("abcabc", nameStorage.name)
+    }
+
+    @Test
+    fun testSimpleRemove() {
+        val nameStorage = NameStorage("abc", 2)
+
+        nameStorage.remove('a', 1)
+
+        assertEquals("bcabc", nameStorage.name)
+    }
+
+    @Test
+    fun testRemoveRepeatableLettersInAWord() {
+        val nameStorage = NameStorage("abca", 2)
+
+        nameStorage.remove('a', 1)
+        nameStorage.remove('a', 2)
+
+        assertEquals("bcabca", nameStorage.name)
+    }
+
+    @Test
+    fun testExampleTest01() {
+        val nameStorage = NameStorage("bac", 2)
+
+        nameStorage.remove('a', 2)
+        nameStorage.remove('b', 1)
+        nameStorage.remove('c', 2)
+
+        assertEquals("acb", nameStorage.name)
+    }
+
+    @Test
+    fun testExampleTest02() {
+        val nameStorage = NameStorage("abacaba", 1)
+
+        nameStorage.remove('a', 1)
+        nameStorage.remove('a', 1)
+        nameStorage.remove('c', 1)
+        nameStorage.remove('b', 2)
+
+        assertEquals("baa", nameStorage.name)
     }
 }
